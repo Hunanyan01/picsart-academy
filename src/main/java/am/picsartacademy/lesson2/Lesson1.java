9b7b204fbb96d306f75f0cdd9ab0e3730d6284a9
Hunanyan01 <hunanyandavid877@gmail.com>
2025-05-12 19:55:36 +0400

    Փոփոխել Lesson1 և Lesson2

--- src/main/java/am/picsartacademy/lesson2/Lesson1.java
+++ src/main/java/am/picsartacademy/lesson2/Lesson1.java
@@ -1,4 +1,34 @@
 package am.picsartacademy.lesson2;
 
 public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("\n\nHello, Picsart Academy!");
+        System.out.println("\nToday we are learning Java.");
+        System.out.println("Here are some important things to remember:");
+        System.out.println("- Always write clean code.");
+        System.out.println("- Use proper indentation.");
+        System.out.println("- Comment your code when necessary.");
+        System.out.print("\n");
+        System.out.println("Good luck and have fun!");
+        System.out.println("\n\nՍտեղծիր Java ծրագիր, որը պարունակում է բոլոր հետևյալ տվյալների տիպերը և տպում է դրանց արժեքները.");
+        byte a = 25;
+        short b = 170;
+        int g = 151000;
+        long d = 300000000L;
+        float o = 65.5f;
+        double z = 72.5;
+        char e = 'M';
+        boolean t = true;
+        String i = "Heriqnaz";
+
+        System.out.println("\n\nAanun" + " - " + i);
+        System.out.println("Tariq" + " - " + a);
+        System.out.println("Bardzrutyun" + " - " + b + " sm");
+        System.out.println("Ashxatavardz" + " - " + g + " dram");
+        System.out.println("Bnakchutyun" + " - " + d);
+        System.out.println("Qash" + " - " + e + " kg");
+        System.out.println("Srti Ashxatanqi Hachaxanutyun" + " - " + z + " zark/rope");
+        System.out.println("Ser" + " - " + o);
+        System.out.println("Hay e?" + " - " + e);
+    }
 }
--- src/main/java/am/picsartacademy/lesson2/Lesson2.java
+++ src/main/java/am/picsartacademy/lesson2/Lesson2.java
@@ -2,7 +2,8 @@ package am.picsartacademy.lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        System.out.println("___________________Homework 1-1_______________________");
+        System.out.println("___________________(Task 1)_______________________");
+        System.out.println("Ստեղծիր զանգված (array) և լրացրու այն երկու թվով։");
         int[] array1 = new int[2];//1)stexcel 2 texani zangvac ev texadrrel tver
 
         array1[0] = 10;
@@ -13,7 +14,8 @@ public class Lesson2 {
         for (int number : array1) { //tpum enq bolor tarery
             System.out.print(number + " ");
         }
-        System.out.println("\n\n___________________Homework 1-2_______________________");
+        System.out.println("\n\n___________________(Task 2)_______________________");
+        System.out.println("Ստեղծիր զանգված և լրացրու այն 1-ից 1000 թվերով (ներառյալ)։");
         int[] array2 = new int[1001];//2)stexcel 1000 texani zangvac ev tal arjeqner
 
         for (int i = 0; i < array2.length; i++) {
@@ -22,7 +24,8 @@ public class Lesson2 {
 
             System.out.println("hasce - " + i + "    zangvaci arjeq - " + array2[i]);
         }
-        System.out.println("\n\n___________________Homework 1-3_______________________");
+        System.out.println("\n\n___________________(Task 3)_______________________");
+        System.out.println("Ստեղծիր զանգված և լրացրու այն կենտ թվերով -20-ից մինչև 20 միջակայքում (ներառյալ)։");
 
         int hashvel = 0; //3) -20 ic 20 tveri mijakayqum gtel em kentery ev tpel
         for (int i = -20; i <= 20; i++) {
@@ -32,7 +35,8 @@ public class Lesson2 {
             }
         }
         System.out.println("\nkent tveri qanaky  " + hashvel + " hat e");
-        System.out.println("\n\n___________________Homework 2-1_______________________");
+        System.out.println("\n\n___________________(Task 4)_______________________");
+        System.out.println("Ստեղծիր զանգված և լցրու այն։ Տպիր բոլոր էլեմենտները, որոնք բաժանվում են 5-ի վրա։");
 
         //Homework 2
         //1)stexcel em zangvac vori arjeqnery bajanvum en 5-i
@@ -44,7 +48,8 @@ public class Lesson2 {
             }
         }
 
-        System.out.println("\n\n___________________Homework 2-2_______________________");
+        System.out.println("\n\n___________________(Task 5)_______________________");
+        System.out.println("Ստեղծիր զանգված և լցրու այն։ Տպիր բոլոր էլեմենտները, որոնք գտնվում են 24.12 և 467.23");
         double[] array4 = {1.2, 1.99, 50.5, 24.12, 24.11, 99.50, 100.40, 10.99, 42.10, 9, 467.23, 50};
 
         for (int i = 0; i < array4.length; i++) {
@@ -52,7 +57,8 @@ public class Lesson2 {
                 System.out.print(array4[i] + " ");
             }
         }
-        System.out.println("\n\n___________________Homework 2-3_______________________");
+        System.out.println("\n\n___________________(Task 6)_______________________");
+        System.out.println("Ստեղծիր զանգված և լցրու այն։ Տպիր այն էլեմենտների քանակը, որոնք բաժանվում են 2-ի վրա։");
 
         int[] array5 = {1, 3, 5, 7, 10, 20, 30, 50, 51, -20, 15, 100, 250, 1000, 1020};
         int hashiv = 0;
@@ -63,7 +69,11 @@ public class Lesson2 {
             }
         }
         System.out.println("zuyg tveri qanakn e - " + hashiv);
-        System.out.println("\n\n___________________Homework 3-1_______________________");
+        System.out.println("\n\n___________________(Task 7)_______________________");
+        System.out.println("Քեզ տրված է ամբողջ թիվ N, որը գտնվում է 0-ից մեծ և 21-ից փոքր միջակայքում (0 < N < 21):\n" +
+                "Պետք է տպես այդ թվի առաջին 10 բազմապատկումները (multiples):\n" +
+                "Յուրաքանչյուր տողում պետք է գրես հետևյալ ձևով՝\n" +
+                "N x i = արդյունք, որտեղ i-ը տատանվում է 1-ից մինչև 10։\n");
 
         int N = 20;
 
