99687a9de434ff3f074d12ff45f90e73b470ea58
Hunanyan01 <hunanyandavid877@gmail.com>
2025-06-19 01:29:03 +0400

    add Lesson 15 (Generics,Varargs)

---
+++ src/main/java/am/picsartacademy/lesson/Lesson15/model/Box.java
@@ -0,0 +1,13 @@
+package am.picsartacademy.lesson.Lesson15.model;
+
+public class Box <T>{
+    private T box1;
+
+    public T getBox1() {
+        return box1;
+    }
+
+    public void setBox1(T box1) {
+        this.box1 = box1;
+    }
+}
---
+++ src/main/java/am/picsartacademy/lesson/Lesson15/model/Pair.java
@@ -0,0 +1,22 @@
+package am.picsartacademy.lesson.Lesson15.model;
+
+public class Pair <K,V> {
+    private K log;
+    private V pas;
+
+    public K getLog() {
+        return log;
+    }
+
+    public void setLog(K log) {
+        this.log = log;
+    }
+
+    public V getPas() {
+        return pas;
+    }
+
+    public void setPas(V pas) {
+        this.pas = pas;
+    }
+}
